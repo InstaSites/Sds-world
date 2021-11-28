4d56b472eb95e94d93ca21a98651f20a3fad210e
YonatanBenezra <yonatanbenezra1@gmail.com>
2021-11-29 00:03:03 +0200

    before deploy

---
+++ config/site.js
@@ -0,0 +1,25 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'SD', // Navigation and Site Title
+  siteTitleAlt: 'SD - Photography', // Alternative Site title for SEO
+  siteTitleShort: 'SD', // short_name for manifest
+  siteHeadline: 'SD', // Headline for schema.org JSONLD
+  siteUrl: 'https://SDs-world.netlify.com', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo.png', // Used for SEO and manifest
+  siteDescription: 'SD - Personal photography portfolio',
+  author: 'SD', // Author for schema.org JSONLD
+  ogLanguage: 'en_US', // og:language
+  googleAnalyticsID: 'UA-134925752-3',
+
+  // Manifest and Progress color
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Your information
+  avatar: '/logos/logo.png',
+  name: 'SD',
+  location: 'United states',
+  socialMedia: [],
+}
